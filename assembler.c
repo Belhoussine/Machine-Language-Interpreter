9ff133d75fed4cae13ad40a0c344545afc87c5bf
Othmane Belhoussine <Othmane.belhou@gmail.com>
2021-03-21 05:39:57 +0100

    Working assembler & working interpreter

---
+++ assembler.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char operation[5], operand1[6], operand2[6], operand1_type, operand2_type;
+} instruction;
+
+//=================START-GLOBAL-VARIABLES==================
+//=========================================================
+
+instruction ASSEMBLY_CODE[10000];
+unsigned short total_instructions = 0;
+char instructions[20][5] = {"ASN",  "ASN", "ADD",  "SUB",  "MUL", "DIV", "SQR",
+                            "SQRT", "EQL", "NEQL", "GOE",  "LOE", "ATV", "VTA",
+                            "JMP",  "LBL", "READ", "PRNT", "STOP"};
+
+//=========================================================
+//==================END-GLOBAL-VARIABLES===================
+
+// --------------------------------------------------------
+
+//=====================START-FUNCTIONS=====================
+//=========================================================
+
+// Convert from assembly operation to opCode
+int operation_to_opcode(char *operation) {
+    for (int i = 0; i < 19; i++) {
+        if (strcmp(operation, instructions[i]) == 0) {
+            return (i % 2) ? -(i / 2) : (i / 2);
+        }
+    }
+}
+
+// Get operand type digit from individual operand types (Literal or Address)
+int get_operands_type(instruction inst) {
+    char op1_type = inst.operand1_type, op2_type = inst.operand2_type;
+
+    if (strcmp(inst.operand1, "AAAA") == 0) {
+        return 7;
+    }
+
+    if (strcmp(inst.operand2, "AAAA") == 0) {
+        if (inst.operand1_type == 'A') {
+            return 8;
+        } else {
+            return 9;
+        }
+    }
+
+    if (op1_type == op2_type) {
+        if (op1_type == 'A') {
+            return 0;
+        } else {
+            return 3;
+        }
+    } else {
+        if (op1_type == 'A') {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
+// Parse lines into tokens
+instruction parse(char *line) {
+    // A line looks like: ASN L0005 A1000
+    instruction inst;
+    char op1[6], op2[6];
+
+    // Divide line into substrings substrings
+    sscanf(line, "%s %s %s", inst.operation, op1, op2);
+
+    memcpy(inst.operand1, op1 + 1, 4);
+    memcpy(inst.operand2, op2 + 1, 4);
+
+    inst.operand1_type = op1[0];
+    inst.operand2_type = op2[0];
+
+    return inst;
+}
+
+// Displaying instruction
+void display(instruction inst) {
+    printf("OP: %s - ", inst.operation);
+    printf("Op1: %s - ", inst.operand1);
+    printf("Op2: %s - ", inst.operand2);
+    printf("Op1Type: %c - ", inst.operand1_type);
+    printf("Op2Type: %c\n", inst.operand2_type);
+}
+
+// Read file and store instructions in RAM (code section)
+void read_source_file(char *file_name) {
+    char line[50];
+    // Open source file (Machine Language)
+    FILE *infp = fopen(file_name, "r");
+    if (infp == NULL) {
+        printf("File not found.");
+        exit(1);
+    }
+
+    // Read line by line
+    while (fgets(line, sizeof(line), infp) != NULL) {
+        ASSEMBLY_CODE[total_instructions] = parse(line);
+        // display(ASSEMBLY_CODE[total_instructions]);
+        total_instructions++;
+    }
+    fclose(infp);
+}
+
+// Translate assembly instruction to machine language instruction
+void translate_instruction(instruction inst, FILE *outfp) {
+    int opcode = operation_to_opcode(inst.operation);
+    char op1[5], op2[5];
+
+    strcpy(op1, inst.operand1);
+    strcpy(op2, inst.operand2);
+
+    // If AAAA is in of the operands, this means that the accumulator will be
+    // used
+    if (strcmp(op1, "AAAA") == 0) {
+        strcpy(op1, "0000");
+    }
+    if (strcmp(op2, "AAAA") == 0) {
+        strcpy(op2, "0000");
+    }
+
+    if (opcode >= 0) {
+        fprintf(outfp, "+");
+    }
+    fprintf(outfp, "%d%s%s%d\n", opcode, op1, op2, get_operands_type(inst));
+}
+
+// Translates from assembly to machine language
+void translate_instructions(char *file_name) {
+    FILE *outfp = fopen(file_name, "w");
+    int i;
+    for (i = 0; i < total_instructions; i++) {
+        translate_instruction(ASSEMBLY_CODE[i], outfp);
+    }
+    fclose(outfp);
+}
+
+//=========================================================
+//======================END-FUNCTIONS======================
+
+int main() {
+    read_source_file("source.al");
+
+    translate_instructions("source.nml");
+    return 0;
+}
--- interpreter.c
+++ interpreter.c
@@ -1,10 +1,9 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-typedef struct
-{
+typedef struct {
     char sign_bit;
     unsigned short operation, operand1, operand2;
     char operands_type;
@@ -27,8 +26,7 @@ int ACC = -1;
 //=========================================================
 
 // Parse lines into tokens
-instruction tokenize(char *line)
-{
+instruction tokenize(char *line) {
     // A line looks like: +0243194875
     instruction inst;
     char op1[5], op2[5];
@@ -48,8 +46,7 @@ instruction tokenize(char *line)
 }
 
 // Displaying instruction
-void display(instruction inst)
-{
+void display(instruction inst) {
     printf("Sign bit: %d \t- ", inst.sign_bit);
     printf("OpCode: %d - ", inst.operation);
     printf("Op1: %d - ", inst.operand1);
@@ -58,312 +55,253 @@ void display(instruction inst)
 }
 
 // Read file and store instructions in RAM (code section)
-void read_source_file(char *file_name)
-{
+void read_source_file(char *file_name) {
     char line[50];
     // Open source file (Machine Language)
     FILE *infp = fopen(file_name, "r");
-    if (infp == NULL)
-    {
+    if (infp == NULL) {
         printf("File not found.");
         exit(1);
     }
 
     // Read line by line
-    while (fgets(line, sizeof(line), infp) != NULL)
-    {
+    while (fgets(line, sizeof(line), infp) != NULL) {
         CODE_MEMORY[total_instructions] = tokenize(line);
         // display(CODE_MEMORY[total_instructions]);
         total_instructions++;
     }
+    fclose(infp);
 }
-
-//=================START-LOG-FUNCTIONS==================
-//=========================================================
-// Log info od assign function
-void assign_log(instruction inst)
-{
-    printf("> [ASSN] Storing %d in %d.\n", inst.operand1, inst.operand2);
-}
-
-// Log info of add_subtract function
-void add_subtract_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> [ADD] Adding %d to %d.\n", inst.operand1, inst.operand2);
-    else
-        printf("> [SUB] Subtracting %d from %d.\n", inst.operand1, inst.operand2);
-}
-
-// Log info of multiply_divide function
-void multiply_divide_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> [MULT] Multiplying %d by %d. \n", inst.operand1, inst.operand2);
-    else
-        printf("> [DIV] Dividing %d by %d. \n", inst.operand1, inst.operand2);
-}
-
-// Log info of square_squareRoot function
-void square_squareRoot_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> Square of %d stored in %d. \n", inst.operand1, inst.operand2);
-    else
-        printf("> Square root of %d stored in %d. \n", inst.operand1, inst.operand2);
-}
-
-// Log info of equals_notEquals function
-void equals_notEquals_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> Compare %d to %d. \n", inst.operand1, inst.operand2);
-    else
-        printf("> Compare %d to %d. \n", inst.operand1, inst.operand2);
-}
-
-// Log info of square_squareRoot function
-void greaterOrEquals_lessThan_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> Square of %d stored in %d.\n", inst.operand1, inst.operand2);
-    else
-        printf("> Square root of %d stored in %d.\n", inst.operand1, inst.operand2);
-}
-
-// Log info of read_print function
-void read_print_log(instruction inst)
-{
-    if (inst.sign_bit == 1)
-        printf("> Reading %d. \n", DATA_MEMORY[inst.operand2]);
-    else
-        printf("> %d\n", inst.operand1);
-}
-
-// Log info of stop function
-void stop_log()
-{
-    printf("> Program is exiting...\n");
-}
-
-//=========================================================
-//==================END-LOG-FUNCTIONS===================
 
 // Transforms instruction's operands from addresses to corresponding literals
-instruction to_literal(instruction inst)
-{
+instruction to_literal(instruction inst) {
     instruction literal_inst = inst;
 
     // Both <OPD1> and <OPD2> are addresses
-    if (inst.operands_type == 0)
-    {
+    if (inst.operands_type == 0) {
         literal_inst.operand1 = DATA_MEMORY[inst.operand1];
         literal_inst.operand2 = DATA_MEMORY[inst.operand2];
-
     }
     // <OPD1> is an address, <OPD2> is a literal
-    else if (inst.operands_type == 1)
-    {
+    else if (inst.operands_type == 1) {
         literal_inst.operand1 = DATA_MEMORY[inst.operand1];
     }
     // <OPD1> is a literal, <OPD2> is an address
-    else if (inst.operands_type == 2)
-    {
+    else if (inst.operands_type == 2) {
         literal_inst.operand2 = DATA_MEMORY[inst.operand2];
     }
 
     return literal_inst;
 }
 
+//=================START-LOG-FUNCTIONS==================
+//=========================================================
+// Log info od assign function
+void assign_log(instruction inst) {
+    if (inst.operands_type == 7) {
+        printf("> [ASN] Storing ACC: %d in %d.\n", ACC, inst.operand2);
+    } else if (inst.operands_type == 8 || inst.operands_type == 9) {
+        printf("> [ASN] Storing %d in ACC.\n", inst.operand1);
+    } else {
+        printf("> [ASN] Storing %d in %d.\n", inst.operand1, inst.operand2);
+    }
+}
+
+// Log info of add_subtract function
+void add_subtract_log(instruction inst) {
+    if (inst.sign_bit == 1)
+        printf("> [ADD] Adding %d to %d.\n", inst.operand1, inst.operand2);
+    else
+        printf("> [SUB] Subtracting %d from %d.\n", inst.operand1,
+               inst.operand2);
+}
+
+// Log info of multiply_divide function
+void multiply_divide_log(instruction inst) {
+    if (inst.sign_bit == 1)
+        printf("> [MULT] Multiplying %d by %d. \n", inst.operand1,
+               inst.operand2);
+    else
+        printf("> [DIV] Dividing %d by %d. \n", inst.operand1, inst.operand2);
+}
+
+// Log info of square_squareRoot function
+void square_squareRoot_log(instruction inst) {
+    if (inst.sign_bit == 1)
+        printf("> Square of %d stored in %d. \n", inst.operand1, inst.operand2);
+    else
+        printf("> Sqrt of %d stored in %d. \n", inst.operand1, inst.operand2);
+}
+
+// Log info of equals_notEquals function
+void equals_notEquals_log(instruction inst) {
+    if (inst.sign_bit == 1)
+        printf("> Compare %d to %d. \n", inst.operand1, inst.operand2);
+    else
+        printf("> Compare %d to %d. \n", inst.operand1, inst.operand2);
+}
+
+// Log info of square_squareRoot function
+void greaterOrEquals_lessThan_log(instruction inst) {
+    if (inst.sign_bit == 1)
+        printf("> Compare %d to %d.\n", inst.operand1, inst.operand2);
+    else
+        printf("> Compare %d to %d.\n", inst.operand1, inst.operand2);
+}
+
+// Log info of stop function
+void stop_log() { printf("> Program is exiting...\n"); }
+
+//=========================================================
+//==================END-LOG-FUNCTIONS===================
+
 // Assign to DATA_MEMORY or ACC
-void assign(instruction inst)
-{
+void assign(instruction inst) {
     instruction literal_inst = to_literal(inst);
 
     // If special operands_type digit {7, 8, 9}
-    if (inst.operands_type == 7)
-    {
+    if (inst.operands_type == 7) {
         DATA_MEMORY[inst.operand2] = ACC;
-        return;
-    }
-    if (inst.operands_type == 8)
-    {
+    } else if (inst.operands_type == 8) {
         ACC = DATA_MEMORY[inst.operand1];
-        return;
-    }
-    if (inst.operands_type == 9)
-    {
+    } else if (inst.operands_type == 9) {
         ACC = inst.operand1;
-        return;
+    } else {  // If normal assign operation, put <OPD1> in <OPD2> (address)
+        DATA_MEMORY[inst.operand2] = literal_inst.operand1;
     }
 
-    // If normal assign operation, put <OPD1> in <OPD2> (address)
-    DATA_MEMORY[inst.operand2] = literal_inst.operand1;
-
-    if (VERBOSE)
-        assign_log(inst);
+    if (VERBOSE) assign_log(inst);
 }
 
 // Add / substract <OPD1> and <OPD2> and put result in ACC
-void add_subtract(instruction inst)
-{
-    if (inst.sign_bit == 1)
-    {
+void add_subtract(instruction inst) {
+    if (inst.sign_bit == 1) {
         ACC = inst.operand1 + inst.operand2;
-    }
-    else
-    {
+    } else {
         ACC = inst.operand1 - inst.operand2;
     }
 
-    if (VERBOSE)
-        add_subtract_log(inst);
+    if (VERBOSE) add_subtract_log(inst);
 }
 
 // Multiply / divide <OPD1> by <OPD2> and put result in ACC
-void multiply_divide(instruction inst)
-{
-    if (inst.sign_bit == 1)
-    {
+void multiply_divide(instruction inst) {
+    if (inst.sign_bit == 1) {
         ACC = inst.operand1 * inst.operand2;
-    }
-    else
-    {
+    } else {
         ACC = inst.operand1 / inst.operand2;
     }
 
-    if (VERBOSE)
-        multiply_divide_log(inst);
+    if (VERBOSE) multiply_divide_log(inst);
 }
 
 // Compute square / square root of <OPD1> and put result in <OPD2>
-void square_squareRoot(instruction inst)
-{
+void square_squareRoot(instruction inst) {
     instruction literal_inst = to_literal(inst);
-    if (inst.sign_bit == 1)
-    {
-        DATA_MEMORY[inst.operand2] = literal_inst.operand1 * literal_inst.operand1;
-    }
-    else
-    {
+    if (inst.sign_bit == 1) {
+        DATA_MEMORY[inst.operand2] =
+            literal_inst.operand1 * literal_inst.operand1;
+    } else {
         DATA_MEMORY[inst.operand2] = sqrt(literal_inst.operand1);
     }
 
-    if (VERBOSE)
-        square_squareRoot_log(inst);
+    if (VERBOSE) square_squareRoot_log(inst);
 }
 
 // Checks if <OPD1> and <OPD2> are equal or different
-void equals_notEquals(instruction inst)
-{
+void equals_notEquals(instruction inst) {
     ACC = inst.operand1 == inst.operand2;
 
-    if (VERBOSE)
-        equals_notEquals_log(inst);
+    if (VERBOSE) equals_notEquals_log(inst);
 }
 
 // Checks if <OPD1> >= <OPD2>
-void greaterOrEquals_lessThan(instruction inst)
-{
+void greaterOrEquals_lessThan(instruction inst) {
     ACC = inst.operand1 >= inst.operand2;
-    if (VERBOSE)
-        greaterOrEquals_lessThan_log(inst);
+    if (VERBOSE) greaterOrEquals_lessThan_log(inst);
 }
 
 // Scan user input and put it in <OPD2>
 // or prints from memory address <OPD1>
-void read_print(instruction inst)
-{
-
+void read_print(instruction inst) {
     instruction literal_inst = to_literal(inst);
     // Read
-    if (inst.sign_bit == 1)
-    {
+    if (inst.sign_bit == 1) {
         printf(">> ");
         scanf("%d", &DATA_MEMORY[inst.operand2]);
     }
     // Print
-    else
-    {
-        printf("> %d\n", literal_inst.operand1);
+    else {
+        printf("> [PRNT] Address %d: %d\n", inst.operand1,
+               literal_inst.operand1);
     }
-
 }
 
 // stop function
-//instruction is maybe not necessary as an input argument
-void stop(instruction inst)
-{
-    if (VERBOSE)
-        stop_log();
+// instruction is maybe not necessary as an input argument
+void stop(instruction inst) {
+    if (VERBOSE) stop_log();
     exit(0);
 }
 // Execute a single instruction in RAM
-void execute_instruction(instruction inst)
-{
-    switch (inst.operation)
-    {
-    case 0:
-        assign(inst);
-        break;
-    case 1:
-        add_subtract(to_literal(inst));
-        break;
-    case 2:
-        multiply_divide(to_literal(inst));
-        break;
-    case 3:
-        square_squareRoot(inst);
-        break;
-    case 4:
-        equals_notEquals(to_literal(inst));
-        break;
-    case 5:
-        greaterOrEquals_lessThan(to_literal(inst));
-        break;
-    case 6:
-        //arrToVar_vatToArr(inst)
-    case 7:
-        //loop_label(inst)
-    case 8:
-        read_print(inst);
-        break;
-    case 9:
-        stop(inst);
-        break;
+void execute_instruction(instruction inst) {
+    switch (inst.operation) {
+        case 0:
+            assign(inst);
+            break;
+        case 1:
+            add_subtract(to_literal(inst));
+            break;
+        case 2:
+            multiply_divide(to_literal(inst));
+            break;
+        case 3:
+            square_squareRoot(inst);
+            break;
+        case 4:
+            equals_notEquals(to_literal(inst));
+            break;
+        case 5:
+            greaterOrEquals_lessThan(to_literal(inst));
+            break;
+        case 6:
+            // arrToVar_vatToArr(inst)
+        case 7:
+            // loop_label(inst)
+        case 8:
+            read_print(inst);
+            break;
+        case 9:
+            stop(inst);
+            break;
     }
 }
 // Execute instructions in RAM
-void execute_instructions()
-{
+void execute_instructions() {
     int IP;
-    for (IP = 0; IP < total_instructions; IP++)
-    {
+    for (IP = 0; IP < total_instructions; IP++) {
         execute_instruction(CODE_MEMORY[IP]);
     }
 }
 
-void debug(){
+void debug() {
     printf("==============DEBUG=============\n");
-    printf(">>> %d %d\n", DATA_MEMORY[2345],DATA_MEMORY[2346]);
+    printf(">>> %d %d\n", DATA_MEMORY[2345], DATA_MEMORY[2346]);
     printf(">>> %d\n", ACC);
 }
 
 //=========================================================
 //======================END-FUNCTIONS======================
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // Getting command line arguments
-    if (argc < 2)
-    {
+    if (argc < 2) {
         printf("Error: you must input a source file.\n");
         printf("[Usage]:\n\t %s <source_file>\n", argv[0]);
-        printf("[Options]:\n\t -v : Verbose (log all executed instructions).\n");
+        printf(
+            "[Options]:\n\t -v : Verbose (log all executed instructions).\n");
         return 1;
-    }
-    else if (argc == 3 && strcmp(argv[2], "-v") == 0)
-    {
+    } else if (argc == 3 && strcmp(argv[2], "-v") == 0) {
         printf("[INFO] Verbose ON.\n");
         VERBOSE = 1;
     }
