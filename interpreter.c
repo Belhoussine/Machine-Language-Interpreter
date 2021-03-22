ef60ceae1149f796836d9e5a539a8ae1ff4f4262
Othmane Belhoussine <Othmane.belhou@gmail.com>
2021-03-22 03:53:18 +0100

    Add 3 tests in AL language

--- interpreter.c
+++ interpreter.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define RESET "\x1B[0m"
+#define BOLD "\x1B[1m"
+
 typedef struct {
     char sign_bit;
     short operation, operand1, operand2;
@@ -16,6 +26,7 @@ instruction CODE_MEMORY[10000];
 int DATA_MEMORY[10000];
 unsigned short total_instructions = 0, IP = 0, VERBOSE = 0;
 int ACC = -1;
+char STDOUT[10000];
 
 //=========================================================
 //==================END-GLOBAL-VARIABLES===================
@@ -99,11 +110,11 @@ instruction to_literal(instruction inst) {
 // Log info od assign function
 void assign_log(instruction inst) {
     if (inst.operands_type == 7) {
-        printf("> [ASN] Storing ACC: %d in %d.\n", ACC, inst.operand2);
+        printf("[ASN] Storing ACC: %d in %d.\n", ACC, inst.operand2);
     } else if (inst.operands_type == 8 || inst.operands_type == 9) {
-        printf("> [ASN] Storing %d in ACC.\n", inst.operand1);
+        printf("[ASN] Storing %d in ACC.\n", inst.operand1);
     } else {
-        printf("> [ASN] Storing %d in address %d.\n", inst.operand1,
+        printf("[ASN] Storing %d in address %d.\n", inst.operand1,
                inst.operand2);
     }
 }
@@ -111,72 +122,77 @@ void assign_log(instruction inst) {
 // Log info of add_subtract function
 void add_subtract_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [ADD] Adding %d to %d, storing result in ACC.\n",
-               inst.operand1, inst.operand2);
+        printf("[ADD] Adding %d to %d, storing result in ACC.\n", inst.operand1,
+               inst.operand2);
     else
-        printf("> [SUB] Subtracting %d from %d, storing result in ACC.\n",
+        printf("[SUB] Subtracting %d from %d, storing result in ACC.\n",
                inst.operand1, inst.operand2);
 }
 
 // Log info of multiply_divide function
 void multiply_divide_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [MUL] Multiplying %d by %d, storing result in ACC.\n",
+        printf("[MUL] Multiplying %d by %d, storing result in ACC.\n",
                inst.operand1, inst.operand2);
     else
-        printf("> [DIV] Dividing %d by %d, storing result in ACC.\n",
+        printf("[DIV] Dividing %d by %d, storing result in ACC.\n",
                inst.operand1, inst.operand2);
 }
 
 // Log info of square_squareRoot function
 void square_squareRoot_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [SQR] Square of %d stored in %d. \n", inst.operand1,
+        printf("[SQR] Square of %d stored in %d. \n", inst.operand1,
                inst.operand2);
     else
-        printf("> [SQRT] Sqrt of %d stored in %d. \n", inst.operand1,
+        printf("[SQRT] Sqrt of %d stored in %d. \n", inst.operand1,
                inst.operand2);
 }
 
 // Log info of equals_notEquals function
 void equals_notEquals_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [EQL] %d == %d. storing result in ACC.\n", inst.operand1,
+        printf("[EQL] %d == %d. storing result in ACC.\n", inst.operand1,
                inst.operand2);
     else
-        printf("> [NEQL] %d != %d, storing result in ACC.\n", inst.operand1,
+        printf("[NEQL] %d != %d, storing result in ACC.\n", inst.operand1,
                inst.operand2);
 }
 
 // Log info of square_squareRoot function
 void greaterOrEquals_lessThan_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [GOE] %d >= %d, storing result in ACC.\n", inst.operand1,
+        printf("[GOE] %d >= %d, storing result in ACC.\n", inst.operand1,
                inst.operand2);
     else
-        printf("> [LT] %d < %d, storing result in ACC.\n", inst.operand1,
+        printf("[LT] %d < %d, storing result in ACC.\n", inst.operand1,
                inst.operand2);
 }
 
 void arrToVar_varToArr_log(instruction inst) {
     if (inst.sign_bit == 1)
-        printf("> [ATV] Storing address %d (array cell) in %d (variable)\n",
+        printf("[ATV] Storing address %d (array cell) in %d (variable)\n",
                inst.operand1, inst.operand2);
     else
-        printf("> [VTA] Storing address %d (variable) in %d (array cell)\n",
+        printf("[VTA] Storing address %d (variable) in %d (array cell)\n",
                inst.operand1, inst.operand2);
 }
 
-void jump_label_log(instruction inst) {
+void jump_label_log(instruction inst, int flag) {
     instruction literal_inst = to_literal(inst);
     if (inst.sign_bit == 1)
-        printf("> [JMP] Jumping to labeled IP %d.\n", literal_inst.operand1);
+        if(flag)
+            printf("[JMP] Jumping to labeled IP %d.\n", literal_inst.operand1);
+        else
+            printf("[JMP] Not jumping.\n");
+
     else
-        printf("> [LBL] Labeling current IP %d in address %d\n", IP, inst.operand2);
+        printf("[LBL] Labeling current IP %d in address %d\n", IP,
+               inst.operand2);
 }
 
 // Log info of stop function
-void stop_log() { printf("> Program is exiting...\n"); }
+void stop_log() { printf("Program is exiting...\n"); }
 
 //=========================================================
 //==================END-LOG-FUNCTIONS===================
@@ -265,7 +281,7 @@ void arrToVar_varToArr(instruction inst) {
         DATA_MEMORY[inst.operand2] = DATA_MEMORY[DATA_MEMORY[inst.operand1]];
     } else {
         // Storing variable (address or literal) in a variable (address)
-        DATA_MEMORY[inst.operand2] = literal_inst.operand1;
+        DATA_MEMORY[DATA_MEMORY[inst.operand2]] = literal_inst.operand1;
     }
 
     if (VERBOSE) arrToVar_varToArr_log(inst);
@@ -283,13 +299,14 @@ void jump_label(instruction inst) {
         DATA_MEMORY[inst.operand2] = IP;
     }
 
-    if (VERBOSE) jump_label_log(inst);
+    if (VERBOSE) jump_label_log(inst, literal_inst.operand2);
 }
 
 // Scan user input and put it in <OPD2>
 // or prints from memory address <OPD1>
 void read_print(instruction inst) {
     instruction literal_inst = to_literal(inst);
+    char ch, str[100];
     // Read
     if (inst.sign_bit == 1) {
         printf(">> ");
@@ -297,8 +314,22 @@ void read_print(instruction inst) {
     }
     // Print
     else {
-        printf("> [PRNT] Address %d: %d\n", inst.operand1,
-               literal_inst.operand1);
+        if (inst.operand2 == 9999) {
+            if (VERBOSE) {
+                // sprintf(STDOUT, "%s\n", STDOUT);
+                ch = '\n';
+                strncat(STDOUT, &ch, 1);
+                printf("[PRNT]\n");
+            } else
+                printf("\n");
+        } else {
+            if (VERBOSE) {
+                sprintf(str, "%d", literal_inst.operand1);
+                strncat(STDOUT, str, sizeof(str));
+                printf("[PRNT]\n");
+            } else
+                printf("%d", literal_inst.operand1);
+        }
     }
 }
 
@@ -309,7 +340,9 @@ void stop(instruction inst) {
 }
 // Execute a single instruction in RAM
 void execute_instruction(instruction inst) {
-    printf("%d", IP+1);
+    if (VERBOSE) {
+        printf(BOLD BLU "[INFO]" RESET GRN "%3d ~ " YEL, IP + 1);
+    }
     switch (inst.operation) {
         case 0:
             assign(inst);
@@ -346,7 +379,13 @@ void execute_instruction(instruction inst) {
 // Execute instructions in RAM
 void execute_instructions() {
     for (IP = 0; IP < total_instructions; IP++) {
+        if (VERBOSE) printf(YEL);
         execute_instruction(CODE_MEMORY[IP]);
+        printf(RESET);
+    }
+    if (VERBOSE) {
+        printf(BOLD GRN "\n=============PROGRAM-OUTPUT===========\n" RESET);
+        printf("%s", STDOUT);
     }
 }
 
@@ -368,7 +407,7 @@ int main(int argc, char *argv[]) {
             "[Options]:\n\t -v : Verbose (log all executed instructions).\n");
         return 1;
     } else if (argc == 3 && strcmp(argv[2], "-v") == 0) {
-        printf("[INFO] Verbose ON.\n");
+        printf(BOLD GRN "[INFO] Verbose ON.\n" RESET);
         VERBOSE = 1;
     }
 
